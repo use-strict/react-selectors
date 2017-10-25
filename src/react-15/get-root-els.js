8fe9c7f9c676eaa46cf62769e60495249f324ede
JLarky <jlarky@gmail.com>
2017-10-25 15:44:33 -0700

    remove everything

--- src/react-15/get-root-els.js
+++
@@ -1,9 +0,0 @@
-/*global document*/
-/*eslint-disable no-unused-vars*/
-function getRootElsReact15 () {
-/*eslint-enable no-unused-vars*/
-    const rootEls      = document.querySelectorAll('[data-reactroot]');
-    const checkRootEls = rootEls.length && Object.keys(rootEls[0]).some(prop => /^__reactInternalInstance/.test(prop));
-
-    return checkRootEls && rootEls || [];
-}
--- test/helpers/service-util.js
+++
@@ -1,7 +0,0 @@
-/*global window*/
-import { t, ClientFunction } from 'testcafe';
-
-export async function loadApp (version) {
-    await ClientFunction(() => window.loadApp(version), { dependencies: { version } })();
-    await t.wait(300);
-}
