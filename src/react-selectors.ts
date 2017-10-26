d55ae47cda7d7d0e03e2b28329db5ae2625c6cc4
JLarky <jlarky@gmail.com>
2017-10-25 17:23:22 -0700

    fix getReactSelector

--- src/react-selectors.ts
+++ src/react-selectors.ts
@@ -2,6 +2,5 @@ import { react16Selector as r16s } from './react-16/get-react'
 
 export const react16Selector = r16s
 
-export const getReactSelector = (roots: Node[]) => (selector: string) => {
+export const getReactSelector = (roots: Node[]) => (selector: string) =>
   react16Selector(roots, selector)
-}
